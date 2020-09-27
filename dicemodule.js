695d7c9005adc4d0db1027828dd97014d23b5462
hUedi <tehoxd@gmail.com>
2020-09-27 16:17:43 +0300

    dicecommit

--- dicemodule.js
+++ dicemodule.js
@@ -1,27 +1,43 @@
 /* eslint-disable no-unused-vars */
+const array = [];
 
-function rollDice(n) {
+//noppa
+const rollDice = (n)=> {
   let x = 0;
   for (i = 0; i < n; i++) {
     x = Math.floor(Math.random()* 6) + 1;
-    taulukko.push(x);
+    array.push(x);
   }
-  return taulukko;
-}
-/*const diceArray = [0, 1, 2, 3, 4, 5, 6];
-const returnNums = rollDice();
-const max = rollDice();
-const min = rollDice();
-const calcAvg = rollDice();
-const n = 10;*/
+  return array;
+};
+rollDice(10);
 
-/*
 //Ensimmäinen funktio (returnNums) palauttaa nopanheiton tulokset merkkijonona jossa numerot on erotettu välilyönnein.
-console.log('Numbers are: ' + returnNums);
+function returnNums(array) {
+  console.log('Numbers are: ' + array);
+  return array;
+}
+returnNums(array);
+
 //Toinen funktio (max) palauttaa suurimman arvon
-console.log('Largest number is ') + (Math.max());
+function maksimi(array) {
+  console.log('Largest number is ' + Math.max(...array));
+  return array;
+}
+maksimi(array);
+
 //kolmas funktio (min)
-console.log('Smallest number is ' + Math.min());
+function minimi(array) {
+  console.log('Smallest number is ' + Math.min(...array));
+  return array;
+}
+minimi(array);
+
 //neljäs funktio (calcAvg) palauttaa kaikkien heittojen keskiarvon.
-console.log('Average is ' + calcAvg);
-*/
+function calcAvg(array) {
+  const total = array.reduce((accumulator, currentValue) => accumulator + currentValue, 0);
+  return total / array.length;
+}
+const average = calcAvg(array);
+console.log('Average is '+ average);
+
